98dc9765f90b04b6218b8b5cb5a32e0087092024
Duc <traungo456@gmail.com>
2025-03-15 00:15:30 +0700

    33:35

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Shop = () => {
+const Hero = () => {
     return (
         <div>
         
@@ -8,4 +8,4 @@ const Shop = () => {
     );
 }
 
-export default Shop;
+export default Hero;
